55b4ee867f956170f31cc589f81e4527c1db1d17
zzhenzeng <zzhenzeng@linewell.com>
2020-07-02 17:31:58 +0800

    add neo4j

---
+++ src/main/java/org/elasticflow/connect/Neo4jConnection.java
@@ -0,0 +1,84 @@
+package org.elasticflow.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.elasticflow.param.pipe.ConnectParams;
+import org.elasticflow.param.warehouse.WarehouseSqlParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Neo4jConnection extends FnConnectionSocket<Connection> {
+
+	private Connection conn = null;
+
+	private final static Logger log = LoggerFactory.getLogger("Neo4j Socket");
+
+	public static FnConnectionSocket<?> getInstance(ConnectParams ConnectParams) {
+		FnConnectionSocket<?> o = new Neo4jConnection();
+		o.init(ConnectParams);
+		o.connect();
+		return o;
+	}
+
+	@Override
+	public boolean connect() {
+		try {
+			if (!status()) {
+				this.conn = DriverManager.getConnection(this.getConnectionUrl());
+				log.info("build connect to " + getConnectionUrl());
+			}
+			return true;
+		} catch (Exception e) {
+			log.error(((WarehouseSqlParam) this.connectParams.getWhp()).getHost() + " connect Exception,", e);
+			return false;
+		}
+	}
+
+	@Override
+	public Connection getConnection(boolean searcher) {
+		int tryTime = 0;
+		try {
+			while (tryTime < 5 && !connect()) {
+				tryTime++;
+				Thread.sleep(2000);
+			}
+		} catch (Exception e) {
+			log.error("try to get Connection Exception,", e);
+		}
+		return this.conn;
+	}
+
+	@Override
+	public boolean status() {
+		try {
+			if (this.conn != null && !this.conn.isClosed()) {
+				return true;
+			}
+		} catch (Exception e) {
+			log.error("get status Exception,", e);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean free() {
+		try {
+			this.conn.close();
+			this.conn = null;
+			this.connectParams = null;
+		} catch (Exception e) {
+			log.error("free connect Exception,", e);
+			return false;
+		}
+		return true;
+	}
+
+	private String getConnectionUrl() {
+		return "jdbc:neo4j:bolt://" + ((WarehouseSqlParam) this.connectParams.getWhp()).getHost() + "/?user="
+				+ ((WarehouseSqlParam) this.connectParams.getWhp()).getUser() + ",password="
+				+ ((WarehouseSqlParam) this.connectParams.getWhp()).getPassword()
+				+ ",scheme=basic,failOverReadOnly=false";
+	}
+
+}
---
+++ src/main/java/org/elasticflow/writer/flow/Neo4jFlow.java
@@ -0,0 +1,141 @@
+package org.elasticflow.writer.flow;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TimeZone;
+
+import org.elasticflow.config.GlobalParam.Mechanism;
+import org.elasticflow.config.InstanceConfig;
+import org.elasticflow.field.EFField;
+import org.elasticflow.model.reader.PipeDataUnit;
+import org.elasticflow.param.end.WriterParam;
+import org.elasticflow.param.pipe.ConnectParams;
+import org.elasticflow.util.FNException;
+import org.elasticflow.writer.WriterFlowSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+public class Neo4jFlow extends WriterFlowSocket {
+	
+	private final static Logger log = LoggerFactory.getLogger("Neo4jFlow");
+	
+	public static Neo4jFlow getInstance(ConnectParams connectParams) {
+		Neo4jFlow o = new Neo4jFlow();
+		o.INIT(connectParams);
+		return o;
+	}
+
+	@Override
+	public boolean create(String instance, String storeId, InstanceConfig instanceConfig) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getNewStoreId(String mainName, boolean isIncrement, InstanceConfig instanceConfig) {
+		if(instanceConfig.getPipeParams().getWriteMechanism()==Mechanism.AB) {
+			return abMechanism(mainName,isIncrement,instanceConfig);
+		}else {
+			return timeMechanism(mainName,isIncrement,instanceConfig);
+		} 
+	}
+
+	@Override
+	public void write(WriterParam writerParam, PipeDataUnit unit, Map<String, EFField> transParams, String instance,
+			String storeId, boolean isUpdate) throws FNException {
+		boolean releaseConn = false;
+		try { 
+			PREPARE(false, false);
+			if (!ISLINK())
+				return;
+			Connection conn = (Connection) GETSOCKET().getConnection(false);
+			try (PreparedStatement statement = conn.prepareStatement(
+					this.getWriteSQL(writerParam, unit, transParams));) {
+				statement.execute();
+			} catch (Exception e) {
+				log.error("PreparedStatement Exception", e);
+			}
+		} catch (Exception e) {
+			log.error("write Exception", e);
+		} finally {
+			REALEASE(false, releaseConn);
+		}
+		
+	}
+
+	@Override
+	public void delete(String instance, String storeId, String keyColumn, String keyVal) throws FNException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeInstance(String instance, String storeId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAlias(String instance, String storeId, String aliasName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flush() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void optimize(String instance, String storeId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private String abMechanism(String mainName, boolean isIncrement, InstanceConfig instanceConfig) { 
+		Connection conn = (Connection) GETSOCKET().getConnection(false);
+		try (PreparedStatement statement = conn.prepareStatement("match(n) return n limit 1");) {
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				try (PreparedStatement statement2 = conn.prepareStatement("match (n) detach delete n");){
+					statement2.execute();
+				} 
+			} 
+		} catch (Exception e) {
+			log.error("clean instance failed!", e); 
+		}  
+		return "a";
+	}
+	
+	private String timeMechanism(String mainName, boolean isIncrement, InstanceConfig instanceConfig) {
+		long current=System.currentTimeMillis(); 
+		return String.valueOf(current/(1000*3600*24)*(1000*3600*24)-TimeZone.getDefault().getRawOffset()); 
+	} 
+	
+	private String getWriteSQL(WriterParam writerParam,PipeDataUnit unit,Map<String, EFField> transParams) { 
+		String tmp = writerParam.getDSL(); 
+		for (Entry<String, Object> r : unit.getData().entrySet()) {
+			String field = r.getKey();
+			EFField transParam = transParams.get(field);
+			if (transParam == null)
+				transParam = transParams.get(field.toLowerCase());
+			if (transParam == null)
+				continue;
+			if(transParam.getIndextype().equals("condition")) {
+				JSONObject sql = JSON.parseObject(tmp);
+				tmp = sql.getString(String.valueOf(r.getValue()));
+			}else {
+				tmp = tmp.replace("#{"+field+"}", String.valueOf(r.getValue()));
+			} 
+			
+		}
+		return tmp;
+	}
+}
