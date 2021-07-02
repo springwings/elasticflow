0e9c43080e36b4f64d61c9ea68a10afa95e696f2
zzhenzeng <zzhenzeng@linewell.com>
2021-07-02 13:48:02 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #OPTS
-JAVA_OPTS="$JAVA_OPTS -server -Xms2g -Xmx2g -Xmn512M -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=8 -XX:+HeapDumpOnOutOfMemoryError"
+JAVA_OPTS="$JAVA_OPTS -server -Xms4g -Xmx4g -Xmn512M -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=8 -XX:+HeapDumpOnOutOfMemoryError"
 
 nohup ${JAVA_HOME}/bin/java ${JAVA_OPTS} -Dconfig=file:/opt/config -jar  ElasticFlow.jar > nohup.txt 2>&1 &
