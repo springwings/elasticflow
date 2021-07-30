91153651e9e77400bd2effe10a21a55ab7089c59
zzhenzeng <zzhenzeng@linewell.com>
2021-07-30 12:22:43 +0800

    remove useless

---
+++ src/main/java/org/elasticflow/writer/handler/WriteHandler.java
@@ -0,0 +1,12 @@
+package org.elasticflow.writer.handler;
+
+/**
+ * Write Handler interface
+ * @author chengwen
+ * @version 4.0
+ * @date 2018-11-14 16:54
+ * 
+ */
+public abstract class WriteHandler{
+
+}
