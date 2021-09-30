d73a41a2e44fdc041a6ad8020e328078ccab376d
springwings <seo@5lulu.com>
2021-09-30 09:02:19 +0800

    update

--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PROC_NAME=elasticflow
+echo "$PROC_NAME start stop..." 
 ps aux | grep java |grep $PROC_NAME | awk '{print $2}' | xargs kill -15
 ps aux | grep java |grep $PROC_NAME | awk '{print $2}'
 sleep 2 #wait kill port
