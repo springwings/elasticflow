6cb85403392b731e1d9db2ae800d6f5d10d03da9
springwings <seo@5lulu.com>
2021-10-12 14:50:40 +0800

    update

--- src/main/java/org/elasticflow/model/EFRequest.java
+++ src/main/java/org/elasticflow/model/EFRequest.java
@@ -1,11 +1,20 @@
+/*
+ * Copyright ElasticFlow B.V. and/or licensed to ElasticFlow B.V. under one
+ * or more contributor license agreements. Licensed under the ElasticFlow License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the ElasticFlow License 2.0 or the Server
+ * Side Public License, v 1.
+ */
 package org.elasticflow.model;
 
 /**
- * ElasticFlow Search Request parameters Model
+ * ElasticFlow Request interface
  * @author chengwen
  * @version 1.0
  * @date 2018-10-22 09:08
  */
 public interface EFRequest {
-
+	
+	public String toString();
+	
 }
