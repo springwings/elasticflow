a193becf096497548304ff37ea073c24b09f6bf8
springwings <seo@5lulu.com>
2022-03-08 10:38:05 +0800

    update

--- stop.sh
+++ stop.sh
@@ -5,4 +5,10 @@ ps aux | grep java |grep $PROC_NAME | awk '{print $2}' | xargs kill -15
 ps aux | grep java |grep $PROC_NAME | awk '{print $2}'
 
 sleep 2 #wait kill port
-echo "$PROC_NAME stop success!" 
+ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`
+while [ $ProcNumber -gt 0 ]
+ do
+   ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`   
+ done 
+echo "$PROC_NAME stop success!"
+
