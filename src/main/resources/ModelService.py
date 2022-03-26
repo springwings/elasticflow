1687ed9dc1e4e2ad8a6dd5750155f2b6a934f442
springwings <seo@5lulu.com>
2022-03-26 12:45:51 +0800

    update

--- src/main/resources/ModelService.py
+++ src/main/resources/ModelService.py
@@ -10,15 +10,16 @@ date:   2022/03/26 11:21
 """
             
 import socket
+import {ENTRANCE}
 
 HOST = '127.0.0.1' 
 _SK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-_SK.bind((HOST,PORT))
+_SK.bind((HOST,{PORT}))
 
 while True:
     try:
-        s.listen()
-        conn,addr = s.accept()
+        _SK.listen()
+        conn,addr = _SK.accept()
         data = conn.recv(1024)
         data = bytes.decode(data)
         result = predict(data)
