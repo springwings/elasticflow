ca9b349bc2209f8b3d887930141dd26d526c1993
springwings <seo@5lulu.com>
2022-10-17 11:03:29 +0800

    update example

--- src/main/java/org/elasticflow/connection/Neo4jConnection.java
+++ src/main/java/org/elasticflow/connection/Neo4jConnection.java
@@ -8,6 +8,12 @@ import org.elasticflow.param.pipe.ConnectParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * use bolt protocol
+ * @author chengwen
+ * @version 1.0
+ * @date 2018-10-26 09:25
+ */
 public class Neo4jConnection extends EFConnectionSocket<Connection> {
 
 	private final static Logger log = LoggerFactory.getLogger("Neo4j Socket");
@@ -58,7 +64,8 @@ public class Neo4jConnection extends EFConnectionSocket<Connection> {
 	}
 
 	private String getConnectionUrl() {
-		return "jdbc:neo4j:bolt://" + this.connectParams.getWhp().getHost() + "/?user="
+		return "jdbc:neo4j:bolt://" + this.connectParams.getWhp().getHost() + ":"+
+				this.connectParams.getWhp().getPort()+ "/?user="
 				+ this.connectParams.getWhp().getUser() + ",password="
 				+ this.connectParams.getWhp().getPassword()
 				+ ",scheme=basic,failOverReadOnly=false";
