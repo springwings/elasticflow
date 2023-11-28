5662ad296a594d51abd3bd77f40edc498b90411c
springwings <seo@5lulu.com>
2023-11-28 10:58:51 +0800

    update

---
+++ src/main/java/org/elasticflow/task/mode/MicroTask.java
@@ -0,0 +1,13 @@
+package org.elasticflow.task.mode;
+
+/**
+ * Micro-task interface 
+ * Common atomic task that run in the system thread pool
+ * Resource.ThreadPools
+ * @author chengwen
+ * @version 1.0
+ * @date 2018-11-13 10:54 
+ */
+public interface MicroTask extends Runnable {
+	
+}
